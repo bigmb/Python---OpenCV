c92b16c703ed24981f21e961d22e0cf07e15d176
Malav Bateriwala <malav.b93@gmail.com>
2018-06-08 16:02:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Digit_recognition",
+    version="0.0.1",
+    description="files for programs",
+    py_modules=["Digit_recognition"],
+    install_requires=["numpy","cv"]
+)
